9df489fd4dcb2f5c387940cdfafc69c7c308b0a6
dmlcn6 <dmlcn6@mail.missouri.edu>
2015-11-09 14:49:57 -0600

    included form request validation

--- application/controllers/formController.php
+++ application/controllers/formController.php
@@ -2,15 +2,38 @@
 
 class FormController extends CI_Controller {
 	function index() {	
+		
+		$this->load->helper(array('form', 'url'));
 		/* -------------------------------------------------
 		   loads the form validation library from 
-		   CodeIgniter for error messages
-		   -----------------------------------------------*/
+		   CodeIgniter for error messages; 
+		   
+		   any resulting error message is placed in <div class="error">
+		---------------------------------------------------*/
 		$this->load->library('form_validation');
+		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+		
+		
+		/* -------------------------------------------------
+	   		Each user submisson field has a validation rule
+	   	-----------------------------------------------*/
+		$this->form_validation->set_rules('username', 'Username', 'required|alpha|min_length[4]|max_length[50]');
+		$this->form_validation->set_rules('pawprint', 'Pawprint', 'required|apha_numeric|exact_length[6]');
+		$this->form_validation->set_rules('title', 'Title', 'required|alpha|max_length[10]');
+		$this->form_validation->set_rules('emp_ID', 'Employee Id', 'required|integer|exact_length[8]');
+		$this->form_validation->set_rules('org', 'Academic Org', 'required|max_length[32]');
+		$this->form_validation->set_rules('address', 'Campus Address', 'required|max_length[100]');
+		$this->form_validation->set_rules('phone', 'Phone Number', 'required|exact_length[10]');
+		$this->form_validation->set_rules('request', 'Type of Request', 'required');		
+
+		$this->form_validation->set_rules('ferpa', 'FERPA Score', 'required|decimal|max_length[6]|less_than[100.01]');		
+		$this->form_validation->set_rules('access', 'Access Description', 'required|max_length[256]');
+		$this->form_validation->set_rules('career[]', 'Academic Careeers', 'required');
+		
 		
 		/* -------------------------------------------------
 		   gather user submission data from name attribute
-		   -----------------------------------------------*/
+		
 		$fullName = $this->input->post("username");
 		$pawPrintSSO = $this->input->post("pawprint");
 		$title = $this->input->post("title");
@@ -19,13 +42,28 @@ class FormController extends CI_Controller {
 		$campusAddress = $this->input->post("address");
 		$phoneNumber = $this->input->post("phone");
 		
-		/* -------------------------------------------------
-		   Each user submisson field has a validation rule
-		   -----------------------------------------------*/
-			$this->form_validation->set_rules('username', 'Username', 'required');
-			$this->form_validation->set_rules('password', 'Password', 'required');
-			$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required');
-			$this->form_validation->set_rules('email', 'Email', 'required');
+		if( $this->post("request") == "new") {
+			$isNew = 1;
+			$isCopy = 0;
+		}
+		else {
+			$isNew = 0;
+			$isCopy = 1;
+		}
+		
+		
+		
+		$ferpaScore = $this->input->post("ferpa");
+		if(ferpaScore > 85)
+			$isValidFERPA = 1;
+		else
+			$isValidFERPA = 0;
+			
+		$accessDesc = $this->input->post("access");
+		
+		
+		
+		--------------------------------------------------*/
 		
 		
 		/* -------------------------------------------------
