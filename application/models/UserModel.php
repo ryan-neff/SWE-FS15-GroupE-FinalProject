c9a9513b50350232fa412dd073b12ad92cd03345
Michael <mbs9b7@mail.missouri.edu>
2015-11-13 14:58:07 -0600

    not sure

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -5,7 +5,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 	function __construct(){
 		parent::__construct();
 		
-		$this->load->database();
+		//$this->load->database();
       }
 
 
